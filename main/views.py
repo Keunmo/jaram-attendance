177d470f73866ce5626a96c8ae3c630c09d444a6
Taewan-P <swimingtw@gmail.com>
2019-04-11 23:59:44 +0900

    Fourth Commit...READ COMMIT COMMENTS
    1. model에서 출석 중복 방지 시스템을 위해서 now를 로컬타임으로 변경했다...이것이 무슨 차이를 주는지 현재로선 확신이 없다 근데 localtime으로 해서 나쁠것이 하나도 없기에 변경했다... 나중에 서버 배포할때도 고려를 하고 뭐 여러모로 생각을 하고 변경을 판단한것.
    2. 출석체크하는데 하루에 두번이상출석하지 못하도록 중복 코드 작성을 완료하였다. 이것은 2월에 하던 attendance-nfc-check에서 구현한 날짜비교 시스템을 조금 가져다가 변경했다... 조금 날짜 비교를 한다고 코드가 길고 조잡해졌지만 현재 나의 실력으로는 이것보다 간단하게 짜는 방법을 모르겠다 ㅠㅠ 일단 코드를 길게는 짰지만 db값 변경하면서 테스트해본결과 아직 헛점은 없다는 사실^^
    
    3. csrf 쿠키
    이제 코드가 어느정도 갖추어 지고 어디서 오류가 나는지 확실히 고치고 확실히 돌아가는 코드가 되었기에 다시 활성화해보았다. 예상했던바와 달리 csrf활성화를 하고 쿠키 넣어서 POST 리퀘 보내보니 아주 잘된다. 그래서 활성화한 상태로 코드를 올린다는 사실!
    
    그럼 오늘은 이만.... 새로운 사실을 알아냈다면 db에는 UTC시간으로 저장되지만 실제로 장고에서 꺼내서 쓸때는 로컬타임으로 변경되서 출력된다는 점이 매우 신기했다...
    
    이번주는 여기까지!! 질문은 카톡으로 오네가이시마수

--- main/views.py
+++ main/views.py
@@ -5,6 +5,8 @@ from django.views.decorators.csrf import csrf_exempt
 
 from .models import Member
 
+import datetime
+
 
 def atd_ranking(request):
     member_lists = Member.objects.order_by('-atd_checked')
@@ -15,8 +17,8 @@ def full_ranking(request):
     member_lists = Member.objects.order_by('-atd_checked')
     return render(request, 'main/full_ranking.html', {'member_lists': member_lists})
 
-# @ensure_csrf_cookie
-@csrf_exempt
+@ensure_csrf_cookie
+# @csrf_exempt
 def atd_check(request):
     if request.method == "POST":
         act_card_id = request.POST.get('card_id')
@@ -27,8 +29,35 @@ def atd_check(request):
         if mem_lookup:
             # Registered
             personnel = mem_lookup
-            personnel.atd_check()
-            print(str(personnel) + '님이 출석에 성공하였습니다.')
+            last_date = personnel.last_checked
+            KST = datetime.timedelta(hours=9)
+            act_last_date = last_date + KST
+            
+            # Duplicated Attendace Checker
+            now = datetime.datetime.now().strftime('%Y-%m-%d').split('-')
+            year_now = now[0]
+            month_now = now[1]
+            day_now = now[2]
+
+            converted_date = act_last_date.strftime('%Y-%m-%d').split('-')
+            year_checked = converted_date[0]
+            month_checked = converted_date[1]
+            day_checked = converted_date[2]
+
+            # Already Checked
+            if day_checked == day_now and \
+                month_checked == month_now and year_checked == year_now:
+
+                print(str(personnel) + '님은 오늘 이미 출석하셨습니다.// ' +
+                str(year_checked) + '년 ' + 
+                str(month_checked) + '월 ' + str(day_checked) + '일에 마지막으로 출석함')
+
+            # Not Checked Today    
+            else:
+                personnel.atd_check()
+                print(str(personnel) + '님이 출석에 성공하였습니다.')
+            
+
             return render(request, 'main/atd_check.html')
         else:
             # Not Registered
