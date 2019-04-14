6366ba23a10a59187d3cd048ff8943d3745f4fde
Keunmo <9858momo@gmail.com>
2019-04-14 16:59:01 +0900

    modify main-views

--- main/views.py
+++ main/views.py
@@ -1,7 +1,6 @@
 from django.shortcuts import render
 from .models import Member
 
-
 def atd_ranking(request):
     member_lists = Member.objects.order_by('-atd_checked')
     member_lists = member_lists[:5]
