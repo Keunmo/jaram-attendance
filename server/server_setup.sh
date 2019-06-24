f1a92bfc0e98069fec63ba1b1ad1fda96315bfa4
Jeongin <jil8885@hanyang.ac.kr>
2019-06-24 19:16:16 +0900

    add secret key generator and install script

---
+++ server/server_setup.sh
@@ -0,0 +1,49 @@
+#! /bin/bash
+
+# install mysql and python headers
+sudo apt install mysql-server libmysqlclient-dev python3-dev
+
+# install required python libraries
+pip3 install --user -r requirements.txt
+
+# mysql root password setting
+# sudo mysql_secure_installation
+
+# input mysql database root account
+read -s -p "input mysql root password : " root_passwd
+echo ''
+read -p "input mysql account to use or make : " new_account
+# sudo mysql -uroot -p$root_passwd mysql -e "$(sudo mysql -uroot -p$root_passwd -sse "SELECT EXISTS(SELECT '1' FROM mysql.user WHERE user = '$new_account')")"
+
+if [ "$RESULT_VARIABLE" = 1 ]; then
+read -s -p "input mysql account password : " new_passwd
+else
+    read -s -p "input mysql account password to make new account: " new_passwd
+    sudo mysql -uroot -p$root_passwd mysql -e "create user $new_account@'localhost' identified by '$new_passwd'"
+fi
+
+echo "This warning is generated when using this script"
+
+# make new database
+
+read -p "Input database name to make : " db_name
+
+# allot previleges to user account
+
+sudo mysql -uroot -p$root_passwd -e "grant all privileges on $db_name.* to $new_account @'localhost' identified by '$new_passwd'"
+
+# secret_key example
+SECRETKEY=$(python3 secretkey_gen.py)
+
+# make setting file
+rm -r .config_secret
+mkdir .config_secret
+echo -e "{\"development\":{\"SECRET_KEY\":\"$SECRETKEY\",\"DATABASES\":{\"default\":{\"ENGINE\": \"django.db.backends.mysql\", \"NAME\": \"$db_name\", \"USER\": \"$new_account\", \"PASSWORD\": \"$new_passwd\", \"HOST\": \"localhost\", \"PORT\":\"\"}}}}" > .config_secret/server_info.json
+
+# migrate database
+
+python3 manage.py makemigrations main
+python3 manage.py migrate
+
+# create supersuer
+python3 manage.py createsuperuser
